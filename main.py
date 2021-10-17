74abf858a351668aa910f0c59370a2f888eb9e84
CernyDavid <91247706+CernyDavid@users.noreply.github.com>
2021-10-17 11:28:27 +0200

    Aktualizovat main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,68 @@
 def on_forever():
     pass
+povoleno = "false"
+
+def stisknutoA():
+    povoleno = "true"
+input.on_button_pressed(Button.A, stisknutoA)
+
+number = 0
+
+def hod():
+    global number
+    number = randint(1, 6)
+    if number == 1:
+        basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        """)
+    else:
+        if number == 2:
+            basic.show_leds("""
+        . . . . .
+        . # . . .
+        . . . . .
+        . . . # .
+        . . . . .
+        """)
+        else:
+            if number == 3:
+                basic.show_leds("""
+                # . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . #
+                """)
+            else:
+                if number == 4:
+                    basic.show_leds("""
+                    . . . . .
+                    . # . # .
+                    . . . . .
+                    . # . # .
+                    . . . . .
+                    """)
+                else:
+                    if number == 5:
+                        basic.show_leds("""
+                        . . . . .
+                        . # . # .
+                        . . # . .
+                        . # . # .
+                        . . . . .
+                        """)
+                    else:
+                        basic.show_leds("""
+                        . # . # .
+                        . . . . .
+                        . # . # .
+                        . . . . .
+                        . # . # .
+                        """)
+
+input.on_gesture(Gesture.SHAKE, hod)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,62 @@
+let povoleno = "false"
+input.onButtonPressed(Button.A, function stisknutoA() {
+    let povoleno = "true"
+})
+let number = 0
+input.onGesture(Gesture.Shake, function hod() {
+    
+    number = randint(1, 6)
+    if (number == 1) {
+        basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    } else if (number == 2) {
+        basic.showLeds(`
+        . . . . .
+        . # . . .
+        . . . . .
+        . . . # .
+        . . . . .
+        `)
+    } else if (number == 3) {
+        basic.showLeds(`
+                # . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . #
+                `)
+    } else if (number == 4) {
+        basic.showLeds(`
+                    . . . . .
+                    . # . # .
+                    . . . . .
+                    . # . # .
+                    . . . . .
+                    `)
+    } else if (number == 5) {
+        basic.showLeds(`
+                        . . . . .
+                        . # . # .
+                        . . # . .
+                        . # . # .
+                        . . . . .
+                        `)
+    } else {
+        basic.showLeds(`
+                        . # . # .
+                        . . . . .
+                        . # . # .
+                        . . . . .
+                        . # . # .
+                        `)
+    }
+    
+})
 basic.forever(function on_forever() {
     
 })
